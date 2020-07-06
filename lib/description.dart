7cd1c9429913b813a8705747017c7702cfc6ceb5
برنامه نویس وب روح الله ترشیزی <R.Torshizi@exir.rayaneh>
2020-07-06 14:18:12 +0430

    create landscape design for description page

--- lib/description.dart
+++ lib/description.dart
@@ -8,34 +8,50 @@ class Description extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "فروشگاه",
-          style: TextStyle(color: Colors.black45, fontFamily: "IranSans"),
+        appBar: AppBar(
+          title: Text(
+            "فروشگاه",
+            style: TextStyle(color: Colors.black45, fontFamily: "IranSans"),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black45,
+          ),
+          elevation: 0,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.arrow_back,
-          color: Colors.black45,
-        ),
-        elevation: 0,
-      ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 15),
-          Align(
+        body: OrientationBuilder(builder: (context, orientation) {
+          return orientation == Orientation.portrait
+              ? verticalUi(context)
+              : horizontalUi(context);
+        }));
+  }
+
+  verticalUi(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(child: SizedBox(height: 15)),
+        Container(
+          child: Align(
             child: Text(
               "Shoes",
               style: TextStyle(color: Colors.red, fontSize: 35),
             ),
           ),
-          Image.network(_product.imageUrl, height: 300, width: 300),
-          Text(_product.productName,
+        ),
+        Container(
+            child: Image.network(_product.imageUrl, height: 300, width: 300)),
+        Container(
+          child: Text(_product.productName,
               style: TextStyle(color: Colors.black, fontSize: 20)),
-          Text(_product.price,
+        ),
+        Container(
+          child: Text(_product.price,
               style: TextStyle(color: Colors.green, fontSize: 20)),
-          Expanded(
+        ),
+        Expanded(
+          child: Container(
             child: Padding(
               padding: EdgeInsets.all(10),
               child: Text(
@@ -48,32 +64,116 @@ class Description extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
+        ),
+        Container(
+          child: Padding(
             padding: const EdgeInsets.only(bottom: 10),
-            child: Align(
-              child: Container(
-                width: MediaQuery.of(context).size.width - 20,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  color: Colors.green,
+            child: Container(
+              child: Align(
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 20,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.green,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      ShoppingData.getInstance().basketItems.add(_product);
+                      print(ShoppingData.getInstance().basketItems);
+                    },
+                    child: Center(
+                        child: Text('افزودن به سبد خرید',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontFamily: 'IranSans'))),
+                  ),
                 ),
-                child: GestureDetector(
-                  onTap: () {
-                    ShoppingData.getInstance().basketItems.add(_product);
-                    print(ShoppingData.getInstance().basketItems);
-                  },
-                  child: Center(
-                      child: Text('افزودن به سبد خرید',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontFamily: 'IranSans'))),
-                ),
+                alignment: Alignment.bottomCenter,
               ),
-              alignment: Alignment.bottomCenter,
             ),
           ),
+        ),
+      ],
+    );
+  }
+
+  horizontalUi(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Row(
+        textDirection: TextDirection.rtl,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child:
+                      Image.network(_product.imageUrl, height: 350, width: 350),
+                  alignment: Alignment.center,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        child: Text(_product.productName,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20)),
+                      ),
+                      Container(
+                        child: Text(_product.price,
+                            style:
+                                TextStyle(color: Colors.green, fontSize: 20)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      _product.description,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                      ),
+                      textDirection: TextDirection.rtl,
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: MaterialButton(
+                    minWidth: MediaQuery.of(context).size.width,
+                    height: 50,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    onPressed: () {
+                      ShoppingData.getInstance().basketItems.add(_product);
+                      print(ShoppingData.getInstance().basketItems);
+                    },
+                    child: Text(
+                      "افزدون به سبد خرید",
+                      style: TextStyle(fontSize: 20, fontFamily: "IranSans"),
+                    ),
+                    color: Colors.green,
+                    highlightColor: Colors.green[200],
+                    textColor: Colors.white,
+                  ),
+                ))
+              ],
+            ),
+          )
         ],
       ),
     );
